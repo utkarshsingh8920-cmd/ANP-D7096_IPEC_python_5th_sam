bf1f0e8ac9d0f322ee9edf0e58f5d5ceb18b7e19
unknown <utkarshsingh8920@gmail.com>
2026-06-25 20:17:16 +0530

    Commit

--- Classwork/Day2/area_of _triangle .py
+++ Classwork/Day2/area_of _triangle .py
@@ -11,11 +11,12 @@ else:
     print ("The base of triangle is = ",base)
     print ("The hight of trangle is = ",height)
     print("Area of Triangle =", 0.5 * base * height)
- """Output :
-    Enter the base of the triangle: 23
-    Enter the height of the triangle: 23
-    --------------------------------------------------
-    The base of triangle is =  23.0
-    The hight of trangle is =  23.0
-    Area of Triangle = 264.5 
+    """Output:
+Enter the base of the triangle: 32
+Enter the height of the triangle: 43
+--------------------------------------------------
+The base of triangle is =  32.0
+The hight of trangle is =  43.0
+Area of Triangle = 688.0
     """
+
