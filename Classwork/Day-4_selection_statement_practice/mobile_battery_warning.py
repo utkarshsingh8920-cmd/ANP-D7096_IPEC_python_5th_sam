7f0a3c30cd378c4f75c479bf28a17560deb03eea
unknown <utkarshsingh8920@gmail.com>
2026-06-27 09:59:29 +0530

    mobile_battery_warning

---
+++ Classwork/Day-4_selection_statement_practice/mobile_battery_warning.py
@@ -0,0 +1,31 @@
+"""--------------------Problem Statement----------------------------------
+A smartphone displays a low battery warning only when the battery percentage falls below 15%. 
+Write a Python program to accept the battery percentage. 
+If the battery is below 15, display: 
+Connect Charger Immediately 
+Otherwise, display nothing.
+------------------------------------------------------------- 
+Sample Input 
+10 
+--------------------------------------------------------------
+Sample Output 
+Connect Charger Immediately 
+"""
+# Smartphone Low Battery Warning
+print("------------------------------------------------")
+battery = int(input("Enter the battery percentage: "))
+if battery<0:
+    exit("Mobile battery can not negative")
+
+if battery < 15:
+    print("Connect Charger Immediately")
+else:
+    print("If you want you can connect charger ")
+
+print("------------------------------------------------")
+"""Output:
+------------------------------------------------
+Enter the battery percentage: 70
+If you want you can connect charger 
+------------------------------------------------
+"""
