d337eb3cc488012ddc35d49c5e1712d731a5c336
unknown <utkarshsingh8920@gmail.com>
2026-07-03 10:38:26 +0530

    print_odd_number_in_tuple

--- Classwork/day_6_tuple/print_odd_number_in_tuple.py
+++ Classwork/day_6_tuple/print_odd_number_in_tuple.py
@@ -2,9 +2,9 @@
 
 number_list=[]
 for x in range (15):
-
+    # input of number 
     num=int(input("Enter numbers : "))
-
+    # inserting into list
     number_list.append(num)
 
 #---------------------------------------------------
@@ -17,5 +17,31 @@ print(number)
 print("---------------------------------------")
 print("Odd number in tuple ")
 for element in number:
+    #for printing odd number 
     if element%2==0:
         print(element,end=',')    
+
+"""
+Output :
+Enter numbers : 3
+Enter numbers : 1
+Enter numbers : 2
+Enter numbers : 3
+Enter numbers : 4
+Enter numbers : 5
+Enter numbers : 6
+Enter numbers : 7
+Enter numbers : 8
+Enter numbers : 9
+Enter numbers : 0
+Enter numbers : 1
+Enter numbers : 2
+Enter numbers : 3
+Enter numbers : 4
+--------------------------------------
+Tuple of a number are
+(3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4)
+---------------------------------------
+Odd number in tuple 
+2,4,6,8,0,2,4,
+"""
