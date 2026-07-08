ffcb221ab543fa1286469eab0776c323743326e6
unknown <utkarshsingh8920@gmail.com>
2026-07-08 19:53:50 +0530

    example1

---
+++ Classwork/day_9_module/example1.py
@@ -0,0 +1,6 @@
+#import module
+from numericcalculater import calculate_addition
+a=5
+b=10
+print("sum of",a,"and",b,"is:",calculate_addition(a,b))
+print("difference of",a,"and",b,"is:",calculate_difference(a,b))
